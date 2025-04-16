8aaa63b79df8cb771d8c94c75ea485927cd85b0f
sleepinghank <xiong-h@foxmail.com>
2025-04-16 23:33:14 +0800

    golang letcode

--- golang/main.go
+++ golang/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
+func main() {
+	nums := []int{3, 2, 4}
+	fmt.Println(twoSum(nums, 6))
 }
